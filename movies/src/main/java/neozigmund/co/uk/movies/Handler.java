8043aa9973d706cc04785e5dcfed7e9e64e048d2
Robert Johnson <the2boys10@gmail.com>
2024-10-03 22:53:32 +0100

    removed non essential files

--- movies/src/main/java/neozigmund/co/uk/movies/Handler.java
+++ movies/src/main/java/neozigmund/co/uk/movies/Handler.java
@@ -7,14 +7,16 @@ import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
 class Handler implements RequestHandler<Request, Response> {
 
-    @Override
-    public Response handleRequest(Request request, Context context) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("Processing question from " + request.name(), LogLevel.INFO);
-        return new Response("Subscribe to Baeldung Pro: baeldung.com/members");
-    }
+	@Override
+	public Response handleRequest(Request request, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("Processing question from " + request.name(), LogLevel.INFO);
+		return new Response("Subscribe to Baeldung Pro: baeldung.com/members");
+	}
+
 }
 
-record Request(String name, String question) {}
+	record Request(String name, String question) {
+	}
 
 record Response(String answer) {}
